285f3529ef47c1fb6607e88e700f886a9cc1a768
Besirat <bisratabebe27@gmail.com>
2020-04-18 14:55:43 -0400

    Initial Commit

---
+++ LocationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationApp
+//
+//  Created by Feaven Maru on 4/18/20.
+//  Copyright © 2020 Besirat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
